921c9e2fee14fc610c1bd5e12e489ecc26fc15f8
Nil Andreu <nilandreug@gmail.com>
2021-04-02 20:23:54 +0200

    Comments on lesson 6

--- Lesson 6 - String Manipulation/BooleanStringTest.py
+++ Lesson 6 - String Manipulation/BooleanStringTest.py
@@ -1,9 +1,9 @@
-print("python".isalnum())
-print("3".isdigit())
+print("python".isalnum()) #This method returns true if all characters are alphanumeric
+print("3".isdigit()) #This method returns true if all characters are digits
 print("3rd".isalnum())
-print("Hii".islower())
+print("Hii".islower()) #This method returns true if all characters are in lowercase
 print("Super".startswith("s"))
-print("Harry Potter".istitle())
+print("Harry Potter".istitle()) #This method returns true if words start with uppercase
 length = "3"
 print("s")
 print(length.isalnum())
--- Lesson 6 - String Manipulation/FormatString.py
+++ Lesson 6 - String Manipulation/FormatString.py
@@ -1,5 +1,5 @@
-print("Python is awesome".capitalize())
-print("PYTHON IS KING".lower())
-print("python is hot".upper())
-print("Harry potter and the half-blood prince".title())
-print("AVENGER:the war of infinity".swapcase())
+print("Python is awesome".capitalize()) #The first word will start in capital letter
+print("PYTHON IS KING".lower()) #All words in minus
+print("python is hot".upper()) #All words in MAYUS
+print("Harry potter and the half-blood prince".title()) #Start of each word in mayus
+print("AVENGER:the war of infinity".swapcase()) #Switch if it is MAYUS or minus at each side of :
